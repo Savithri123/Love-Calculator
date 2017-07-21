7066993d7ce6dcf75a3a0b2eb34a0895c2db6030
Savithri123 <snsavithrik1@gmail.com>
2017-07-21 19:54:19 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  name1= '';
+  name2= '';
 
+  get score(){
+    const letters = (this.name1 + this.name2).toLocaleLowerCase();
+    let sum = 0;
+    for(let i = 0; i<letters.length; i++){
+      sum += letters.charCodeAt(i);
+
+    }
+      return sum % 101;
   }
+
 
 }
